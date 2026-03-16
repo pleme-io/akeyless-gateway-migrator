0b97959b18394d66cbaf28262af8ad4609b97033
drzzln <drzzln@protonmail.com>
2026-03-15 21:09:15 -0400

    refactor: use substrate repo-flake.nix universal builder
    Replaces bespoke flake.nix boilerplate with declarative config via
    substrate's repo-flake.nix. All build logic, devShell tooling, and
    meta attributes derived from (language, builder) dispatch.

--- flake.nix
+++ flake.nix
@@ -10,22 +10,15 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, substrate, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
-      mkGoTool = (import "${substrate}/lib/go-tool.nix").mkGoTool;
-    in {
-      packages.default = mkGoTool pkgs {
-        pname = "akeyless-gateway-migrator";
-        version = "0.0.0-dev";
-        src = self;
-        vendorHash = "sha256-E49fPxp844OnTwSJSLnhNG5lu0W9UB8I1Wq8hE+1fo8=";
-        description = "CLI tool to migrate configuration between Akeyless Gateway clusters";
-        homepage = "https://github.com/pleme-io/akeyless-gateway-migrator";
-      };
-
-      devShells.default = pkgs.mkShellNoCC {
-        packages = with pkgs; [ go gopls gotools ];
-      };
-    });
+  outputs = inputs: (import "${inputs.substrate}/lib/repo-flake.nix" {
+    inherit (inputs) nixpkgs flake-utils;
+  }) {
+    self = inputs.self;
+    language = "go";
+    builder = "tool";
+    pname = "akeyless-gateway-migrator";
+    vendorHash = "sha256-E49fPxp844OnTwSJSLnhNG5lu0W9UB8I1Wq8hE+1fo8=";
+    description = "CLI tool to migrate configuration between Akeyless Gateway clusters";
+    homepage = "https://github.com/pleme-io/akeyless-gateway-migrator";
+  };
 }
